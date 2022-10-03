272b78580f3c48f18d2195d26f24abb52fd4cff4
chiman <rlaclghks123@naver.com>
2022-10-03 22:45:27 +0900

    header 완료

---
+++ src/routes/Home.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100vw",
+        margin: "70px 0px",
+      }}
+    >
+      Home
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/styled.d.tsx
@@ -0,0 +1,14 @@
+import "styled-components";
+
+export interface DefaultTheme {
+  red: string;
+  black: {
+    veryDark: string;
+    darker: string;
+    lighter: string;
+  };
+  white: {
+    darker: string;
+    lighter: string;
+  };
+}

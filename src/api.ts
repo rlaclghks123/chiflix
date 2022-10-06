115ba29f5a012817a847d3fe3648f8e5f2f80be0
chiman <rlaclghks123@naver.com>
2022-10-06 23:05:04 +0900

    slider 생성

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,24 @@
 export const API_KEY = "6e95db29c8f6051ed76f375c99c01040";
 export const BASE_URL = "https://api.themoviedb.org/3";
 
+interface IMovie {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+export interface IGetMoviesResult {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: IMovie[];
+  total_pages: number;
+  total_results: number;
+}
+
 export const getMovies = () => {
   return fetch(`${BASE_URL}/movie/now_playing?api_key=${API_KEY}`).then(
     response => response.json()
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,30 +1,13 @@
-import React from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import React, { useState } from "react";
 import { useQuery } from "react-query";
 import styled from "styled-components";
-import { getMovies } from "../api";
+import { getMovies, IGetMoviesResult } from "../api";
 import { makeImgPath } from "../utils";
-
-interface IMovie {
-  id: number;
-  backdrop_path: string;
-  poster_path: string;
-  title: string;
-  overview: string;
-}
-
-export interface IGetMovieResult {
-  dates: {
-    maximum: String;
-    minimum: String;
-  };
-  page: number;
-  results: IMovie[];
-  total_pages: number;
-  total_results: number;
-}
 
 const Wrapper = styled.div`
   background-color: black;
+  margin-bottom: 200px;
 `;
 
 const Loader = styled.div`
@@ -34,33 +17,77 @@ const Loader = styled.div`
   height: 20vh;
 `;
 
-const Banner = styled.div<{ bgPhoto: string }>`
-  height: 100vh;
+const Banner = styled.div<{ bgphoto: string }>`
+  height: 80vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
   padding: 60px;
   background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
-    url(${props => props.bgPhoto});
+    url(${props => props.bgphoto});
   background-size: cover;
 `;
 
 const Title = styled.h2`
-  font-size: 70px;
-  margin-bottom: 15px;
+  font-size: 68px;
+  margin-bottom: 20px;
 `;
 
 const OverView = styled.p`
   font-size: 25px;
   width: 50%;
-  line-height: 35px;
+  line-height: 30px;
 `;
 
+const Slider = styled.div`
+  top: -100px;
+  position: relative;
+`;
+
+const Row = styled(motion.div)`
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  gap: 5px;
+  width: 100%;
+  position: absolute;
+`;
+
+const Box = styled(motion.div)<{ bgphoto: string }>`
+  height: 200px;
+
+  background-image: url(${props => props.bgphoto});
+  background-size: cover;
+  background-position: center;
+  font-size: 66px;
+`;
+
+const rowVari = {
+  hidden: { x: window.outerWidth + 5 },
+  visible: { x: 0 },
+  exit: { x: -window.outerWidth - 5 },
+};
+const offset = 6;
 function Home() {
-  const { data, isLoading } = useQuery<IGetMovieResult>(
+  const { data, isLoading } = useQuery<IGetMoviesResult>(
     ["movies", "nowPlaying"],
     getMovies
   );
+
+  const [index, setIndex] = useState(0);
+
+  const [leaving, setLeaving] = useState(false);
+
+  const toggleLeaving = () => setLeaving(prev => !prev);
+
+  const toggelBtn = () => {
+    if (data) {
+      if (leaving) return;
+      toggleLeaving();
+      const totalMovie = data.results.length - 1;
+      const MaxIndex = Math.floor(totalMovie / offset) - 1;
+      setIndex(prev => (prev === MaxIndex ? 0 : prev + 1));
+    }
+  };
 
   return (
     <Wrapper>
@@ -68,10 +95,35 @@ function Home() {
         <Loader>Loading...</Loader>
       ) : (
         <>
-          <Banner bgPhoto={makeImgPath(data?.results[1].backdrop_path || "")}>
+          <Banner
+            onClick={toggelBtn}
+            bgphoto={makeImgPath(data?.results[1].backdrop_path || "")}
+          >
             <Title>{data?.results[0].title}</Title>
             <OverView>{data?.results[0].overview}</OverView>
           </Banner>
+          <Slider>
+            <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
+              <Row
+                variants={rowVari}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                transition={{ type: "tween", duration: 1 }}
+                key={index}
+              >
+                {data?.results
+                  .slice(1)
+                  .slice(offset * index, offset * index + offset)
+                  .map(movie => (
+                    <Box
+                      bgphoto={makeImgPath(movie.backdrop_path || "", "w500")}
+                      key={movie.id}
+                    ></Box>
+                  ))}
+              </Row>
+            </AnimatePresence>
+          </Slider>
         </>
       )}
     </Wrapper>
